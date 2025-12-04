69145ff79065589377db77817f073f6525852a17
Mathew Bushell <mathew.bushell@gmail.com>
2025-12-04 11:16:31 -0500

    added file selection and json dump to watcher

--- watcher.py
+++ watcher.py
@@ -1,7 +1,21 @@
 import subprocess
 import os
 import time
+import glob
+import json
 
 photoshop_path = r"C:\Program Files\Adobe\Adobe Photoshop 2025\Photoshop.exe"
+arw_files = glob.glob(r"D:\photos\sony_a6000\raw_test\*.ARW")
 
-subprocess.Popen([photoshop_path])
+with open("queue.json", "w") as f:
+    json.dump({"files": arw_files}, f)
+
+try:
+    process = subprocess.Popen([photoshop_path])
+    print('Opening Photoshop...')
+
+except FileNotFoundError:
+    print(f'Error: Could not find Photoshop at {photoshop_path}')
+    print('Please check the path and try again.')
+
+
